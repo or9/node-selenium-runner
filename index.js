4df0556028d6744d3d71db6ff0e6e33641c5598d
Abdur Rahman <durmon.albasir@gmail.com>
2018-03-04 20:54:25 -0500

    working chromedriver

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 #!/usr/bin/env node
 "use strict";
 
-const { Builder, By, Key, until } = require("selenium-webdriver");
+const { Builder, By, Capabilities, Key, until } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
 const { red, green } = require("ansi-colors");
+const path = require("chromedriver").path;
+
+const service = new chrome.ServiceBuilder(path).build();
+chrome.setDefaultService(service);
+// const casper = require("casper").create();
+
+// casper.start("http://casperjs.org")
+// 	.then(() => {
+// 		console.log("done it");
+// 	})
+
+// casper.run();
+
+// process.env.PATH = `${process.env.PWD}/node_modules/.bin:${process.env.PATH}`;
+// console.log("path???", process.env.PATH);
 
 (async function () {
-	var driver = await new Builder().forBrowser("chrome").build();
+	var driver = await new Builder()
+		.withCapabilities(Capabilities.chrome())
+		.build();
 
 	try {
 		console.log(green.open, "running...", green.close);
