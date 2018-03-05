7bcf7cdd3a8ff48baaa1da6f00ea5f2d1c0a704e
Abdur Rahman <durmon.albasir@gmail.com>
2018-03-04 21:06:32 -0500

    use less crazy then

--- index.js
+++ index.js
@@ -9,38 +9,32 @@ const path = require("chromedriver").path;
 const service = new chrome.ServiceBuilder(path).build();
 chrome.setDefaultService(service);
 
-(async function () {
-	var driver = await new Builder()
-		.withCapabilities(Capabilities.chrome())
-		.build();
+// (async function () {
+// 	var driver = await new Builder()
+// 		.withCapabilities(Capabilities.chrome())
+// 		.build();
 
-	try {
-		console.log(green.open, "running...", green.close);
-		await driver.get("http://www.google.com/ncr");
-		await driver.findElement(By.name("q")).sendKeys("webdriverrrr", Key.RETURN);
-		await driver.wait(until.titleIs("webdriverrrr - Google Search"), 1000);
-	} catch (err) {
-		console.error(red.open, err, red.close);
-	} finally {
-		await driver.quit();
-	}
-})();
-
-// var driver = new Builder()
-// 	.withCapabilities(Capabilities.chrome())
-// 	.build();
-
-// driver.get("http://www.google.com/ncr")
-// 	.then(() => {
-// 		return driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN);
-// 	})
-// 	.then(() => {
-// 		return driver.wait(until.titleIs("webdriver - Google Search"), 1000);
-// 	})
-	// .then(() => {
-	// 	driver.quit();
-	// })
-// 	.catch(err => {
+// 	try {
+// 		console.log(green.open, "running...", green.close);
+// 		await driver.get("http://www.google.com/ncr");
+// 		await driver.findElement(By.name("q")).sendKeys("webdriverrrr", Key.RETURN);
+// 		await driver.wait(until.titleIs("webdriverrrr - Google Search"), 1000);
+// 	} catch (err) {
 // 		console.error(red.open, err, red.close);
-		// driver.quit();
-	// })
+// 	} finally {
+// 		await driver.quit();
+// 	}
+// })();
+
+var driver = new Builder()
+	.withCapabilities(Capabilities.chrome())
+	.build();
+
+driver.get("http://www.google.com/ncr")
+	.then(() => driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN))
+	.then(() =>  driver.wait(until.titleIs("webdriver - Google Search"), 1000))
+	.then(() => driver.quit())
+	.catch(err => {
+		console.error(red.open, err, red.close);
+		driver.quit();
+	})
